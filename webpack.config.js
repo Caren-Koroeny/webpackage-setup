02a0af8bdd8bd91d48bfaf51b414161337f57ba5
Caren-Koroeny <carensiya5@gmail.com>
2023-01-18 12:21:59 +0300

    added webpack files

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode:'development',
+
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+
+  output:{
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist'),    
+  },
+
+  module: {
+        rules:[
+    {
+        test:/\.css$/,
+        use:["style-loader", "css-loader"]
+    }
+  ]
+},
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+
+};
